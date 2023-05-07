0878e808758d01684dbfc450b69fa0b3fa20aaf8
Simon Alphonse <Simon.Alphonse@Ideas2it.com>
2023-05-08 03:09:52 +0530

    Completed ToDo App (#16)
    Co-authored-by: Simon Alphonse <IAmSimon@outlook.in>

--- todo/account/models.py
+++ todo/account/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Account(models.Model):
-    username = models.CharField(max_length=100, blank=False)
-    # email = models.EmailField(blank=False)
-    # password = models.CharField(max_length=30, blank=False)
-
-    def __str__(self) -> str:
-        return f"{self.username}"
