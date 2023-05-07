d4cab2bea2f7fe497431b88baa3e21922b331e7f
Simon Alphonse <Simon.Alphonse@Ideas2it.com>
2023-05-08 03:53:46 +0530

    fixed few bugs (#17)
    Co-authored-by: Simon Alphonse <IAmSimon@outlook.in>

--- todo/account/forms.py
+++ todo/account/forms.py
@@ -15,24 +15,3 @@ class CustomUserCreationForm(UserCreationForm):
             "password1",
             "password2",
         ]
-
-    def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields["first_name"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Enter first name..."}
-        )
-        self.fields["last_name"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Enter last name..."}
-        )
-        self.fields["email"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Enter email address..."}
-        )
-        self.fields["username"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Enter username..."}
-        )
-        self.fields["password1"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Enter password..."}
-        )
-        self.fields["password2"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "Confirm password..."}
-        )
