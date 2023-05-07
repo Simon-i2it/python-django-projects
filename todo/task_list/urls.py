91d9f43ac85d13ee4b5db388fc8bee40cd794396
Simon Alphonse <Simon.Alphonse@Ideas2it.com>
2023-05-08 04:20:19 +0530

    fixed delete task list (#18)
    Co-authored-by: Simon Alphonse <IAmSimon@outlook.in>

--- todo/task_list/urls.py
+++ todo/task_list/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path("task/<pk>/delete", views.DeleteTask.as_view(), name="url-delete-task"),
     path("tasks/delete", views.delete_tasks, name="url-delete-tasks"),
     path(
+        "tasks/confirm_delete",
+        views.confirm_delete_tasks,
+        name="url-confirm-delete-tasks",
+    ),
+    path(
         "task/<pk>/<is_completed>",
         views.complete_task,
         name="url-complete-task",
