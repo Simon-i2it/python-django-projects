545dcf87a611c400f51ec8fb206a0696f182691d
Simon Alphonse <Simon.Alphonse@Ideas2it.com>
2023-05-09 03:11:15 +0530

    Blog dev (#21)
    * Added favicon
    
    * Fixed Models
    
    ---------
    
    Co-authored-by: Simon Alphonse <IAmSimon@outlook.in>

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.welcome),
     path("welcome", views.welcome, name="url-welcome"),
     path("posts", views.posts, name="url-posts"),
+    path("post/new", views.NewPost.as_view(), name="url-new-post"),
     path("post/<slug:slug>", views.post, name="url-post"),
 ]
