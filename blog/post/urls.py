3f893bd2b0fb0e8a85af8ea3ef5319b01ff74038
Simon Alphonse <Simon.Alphonse@Ideas2it.com>
2023-05-11 10:21:40 +0530

    Added Edit and Delete Post (#26)
    Co-authored-by: Simon Alphonse <IAmSimon@outlook.in>

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("posts", views.posts, name="url-posts"),
     path("post/new", views.new_post, name="url-new-post"),
     path("post/<int:pk>", views.post, name="url-post"),
+    path("post/<int:pk>/edit", views.UpdatePost.as_view(), name="url-edit-post"),
+    path("post/<int:pk>/delete", views.delete_post, name="url-delete-post"),
 ]
